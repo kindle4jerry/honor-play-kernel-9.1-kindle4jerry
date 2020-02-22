bca6951b7ddc3d4e3f9e287b9206fcdafd87ab4a
kindle4jerry <kindle4jerry@gmail.com>
2020-02-22 17:28:47 +0800

    change the toolchain

--- kernel/build.sh
+++ kernel/build.sh
@@ -2,15 +2,13 @@
 
 #开头环境配置，可以参考官方文档，我在此简单汉化搬运，也可以把我的路径改改
 #你需要想想办法比如谷歌搜索去搞一个aarch64-linux-android-4.9这个玩意，然后把下面的路径改一改
-#ps谷歌说他官方remove了这个工具，我觉得是删除把
-#可以考虑从我的服务器搞一个aarch啥的
-#一步到位的文件连接 http://172.83.154.60/usr/uploads/pans/aarch64.tar.gz
-#小网站，貌似没啥防护，做人要厚道啊，我这无广告免费满速下载啊，别有事没事来捶我服务器就行。
-#更换服务器IP，再换的话，可以访问一下我小破站https://kindle4jerry.github.io/如果还活着的话可以在分类/文件里面轻松翻到这个玩意
-#还不行可以戳我，github看的少不及时直接戳我企鹅号915075436
+#但是谷歌remove了这个工具里面的GCC所以现在新版本是不能用的所以可以去
+#https://github.com/kindle4jerry/aarch64-linux-android-4.9-bakup
+#获取一个旧版本的备份
+
 #下面第一行根据你的aarch的位置设置，第二行照抄
 
-export PATH=$PATH:/home/lumia/aarch64-linux-android-4.9/bin
+export PATH=$PATH:/home/lumia/aarch64-linux-android-4.9-bakup/bin
 export CROSS_COMPILE=aarch64-linux-android-
 
 ##以下是官方文档给出的命令 看不懂可以不改
