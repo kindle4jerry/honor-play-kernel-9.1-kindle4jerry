00993e3e20cc4996667f1aeff43311376cb705a5
kindle4jerry <kindle4jerry@gmail.com>
2020-05-02 01:32:35 +0800

    fix L3CACHE build error

--- include/linux/hisi/mntn_l3cache_ecc.h
+++ include/linux/hisi/mntn_l3cache_ecc.h
@@ -4,9 +4,9 @@
 
 #ifndef _MNTN_L3CACHE_ECC_H
 #define _MNTN_L3CACHE_ECC_H
-#ifdef CONFIG_HISI_L3CACHE_ECC
-extern void l3cache_ecc_get_status(u64 *err1_status, u64* err1_misc0, int need_print);
-#else
-extern inline void l3cache_ecc_get_status(u64 *err1_status, u64* err1_misc0, int need_print) {};
-#endif
+//#ifdef CONFIG_HISI_L3CACHE_ECC
+//extern void l3cache_ecc_get_status(u64 *err1_status, u64* err1_misc0, int need_print){};
+//#else
+//extern void l3cache_ecc_get_status(u64 *err1_status, u64* err1_misc0, int need_print) {};
+//#endif
 #endif /* _MNTN_H_ */
